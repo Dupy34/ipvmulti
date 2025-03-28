974e79061d27b952171bcc3bce98f1a0ce1224c9
Dupy34 <133169378+Dupy34@users.noreply.github.com>
2025-03-28 10:49:49 -0600

    Overlap LaunchPad String

--- Source/Ipvmulti/Private/Actors/LaunchPad.cpp
+++ Source/Ipvmulti/Private/Actors/LaunchPad.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Ipvmulti/Public/Actors/LaunchPad.h"
 
 #include "Components/BoxComponent.h"
+#include "GameFramework/Character.h"
 
 
 // Sets default values
@@ -19,7 +19,19 @@ ALaunchPad::ALaunchPad()
 void ALaunchPad::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	OverlapComp -> OnComponentBeginOverlap.AddDynamic(this, &ALaunchPad::OverlapLaunchpad);
+}
+
+void ALaunchPad::OverlapLaunchpad(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+	ACharacter* MyCharacter = Cast<ACharacter>(OtherActor);
+	if (MyCharacter)
+	{
+		if (GEngine)
+		{
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, TEXT("Overlap"));
+		}
+	}
 }
 
 // Called every frame
